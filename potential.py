41dd9dcc08ef19a69b1d0042cb61871d0838ee07
hannasg22 <hannastammer22@gmail.com>
2024-05-16 18:49:43 +0200

    Added file to define the potential.

---
+++ potential.py
@@ -0,0 +1,9 @@
+"""This module will contain the potential used to solve our equations.
+
+Parameters needed in the potentials:
+    r0 : r value for the range of the square well potential (dimensions: fm)
+    V_C0 : depth of the potential (dimensions: MeV)
+These values have been chosen due to experimental data.
+
+For further information check README.md section.
+"""
