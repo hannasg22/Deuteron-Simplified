8b3c74f00d962a900d39706d0d1980807d571651
hannasg22 <hannastammer22@gmail.com>
2024-05-16 19:00:14 +0200

    Created a code which will get us the values that we want from the jsonlines file.

---
+++ get_values.py
@@ -0,0 +1,63 @@
+"""This module will define all variables described in the values_deuteron.jsonl
+to later use them in the potential.py, deuteron.py and run.py files.
+We will have functions getting the values of:
+    - Potential depth and range
+    - Initial conditions
+    - Boundary conditions
+    - Range of radius r
+    - Energy guess value
+"""
+
+import jsonlines
+
+def initial_conditions():
+    file_name = "values_deuteron.jsonl"
+    init_cond = [] # Form of the vector will be [us_0, vs_0, ud_0, vd_0]
+
+    # Open file and read initial conditions
+    with jsonlines.open(file_name, mode='r') as reader:
+        for line in reader:
+            if "Initial conditions" in line:
+                if "us_0" in line:
+                    init_cond.append(line["us_0"])
+                if "vs_0" in line:
+                    init_cond.append(line["vs_0"])
+    return init_cond
+   
+def boundary_conditions():
+    file_name = "values_deuteron.jsonl"
+    bound_cond = []
+
+    # Open file and read boundary conditions
+    with jsonlines.open(file_name, mode='r') as reader:
+        for line in reader:
+            if "Boundary condition" in line:
+                bound_cond.append(line)
+
+    us_fin = bound_cond[0]['us_fin']
+    return us_fin
+
+def range_of_radius():
+    file_name = "values_deuteron.jsonl"
+    r_range = []
+
+    # Open file and read values for minimum and maximum r
+    with jsonlines.open(file_name, mode='r') as reader:
+        for line in reader:
+            if "Range of radius" in line:
+                if "r_initial" in line:
+                    r_range.append(line["r_initial"])
+                if "r_final" in line:
+                    r_range.append(line["r_final"])
+    return r_range
+
+def energy_guess():
+    file_name = "values_deuteron.jsonl"
+
+    # Open file and read first guess of E value
+    with jsonlines.open(file_name, mode='r') as reader:
+        for line in reader:
+            if "Energy guess" in line:
+                E_guess = line["Energy guess"]
+    return E_guess
+
