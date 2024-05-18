46136b9d7a52a272d4c93ad4992be07697ed6b55
hannasg22 <hannastammer22@gmail.com>
2024-05-18 13:14:12 +0200

    Divided find_energy.py file into two parts. One which just has the root finding process (find_energy.py) and other which just gets us the final results and plots them (run.py).

--- find_energy.py
+++ find_energy.py
@@ -1,13 +1,11 @@
 """Here we define the root finding process.
 
 We take the function from 'schro_equation.py', which has the system we need to
-solve. We then use an integration process that will first be solved with the
-initial guess we made for the energy eigenvalue (defined in 'generate_data.py'
-and saved in 'values_deuteron.jsonl'.
+solve. We then use an integration process that gives the function lg the system
+of equations with the given parameters.
 
-So, to follow this process we will use two functions implemented by SciPy:
-    - solve_ivp --> to find the eigenvalue from the equation system
-    - root_scalar --> to find the E value which obeys the boundary condition
+So, to follow this process we will use the functions from SciPy:
+    - solve_ivp --> to solve the equation with the introcued energy value
 """
 
 import numpy as np
@@ -24,45 +22,25 @@ def error_E(E_guess):
     desired boundary condition.
 
     For that we solve the system of equations with E_guess and compare the
-    result in the ending point with the desired boundary condition.
+    result in the ending point with the desired boundary condition us_fin.
 
     Inputs:
         E: value of energy for which we solve the equations
 
     Output:
         error: difference between the final point value with E_guess and the
-        actual value we want to achieve
+               actual value we want to achieve
     """
-    
+
+    # Get the conditions to solve the problem
     init_cond = get.initial_conditions()
     us_fin = get.boundary_condition()
     r_range = get.range_of_radius()
-    
+
+    # Solve for E_guess
     solution_guess = solve_ivp(lambda r, y: eq.radial_equation(r, y, E_guess), r_range, init_cond, method = 'RK45', max_step = 0.01)
+
     # Difference between solution with E_guess and the actual value we want
     error = solution_guess.y[0][-1] - us_fin
+
     return error
-
-energy_value = root_scalar(error_E, method='secant', x0=get.energy_guess())
-E_binding = energy_value.root
-print(E_binding)
-
-# Get the eigenfunction with proper E value
-solution_final = solve_ivp(lambda r, y: eq.radial_equation(r, y, E_binding), get.range_of_radius(), get.initial_conditions(), method = 'RK45', max_step = 0.01)
-
-# To visualize u(r) vs. r and V(r)
-r_values = solution_final.t
-u_values = solution_final.y[0]
-V_values = [pot.V_c_squarewell(r) for r in r_values]
-
-# Graph of function u(r), V(r) and E
-plt.figure()
-plt.plot(r_values, 20 * u_values, label='u(r)')
-plt.plot(r_values, V_values, label='Potential V(r) (MeV)')
-plt.axhline(y=E_binding, color='r', linestyle='--', label='Binding energy E (MeV)')
-plt.xlabel('r (fm)')
-plt.ylabel('u(r)')
-plt.title('Eigenfunction u(r) vs. ditance r')
-plt.legend()
-plt.grid(True)
-plt.show()
---
+++ run.py
@@ -0,0 +1,46 @@
+"""Results will be obtained through this part of the code. Here we define the
+root finding process.
+
+To follow this process we will use two functions implemented by SciPy:
+    - root_scalar --> to find the E value which obeys the boundary condition
+    - solve_ivp --> to get the eigenfunction with the proper E value
+
+So, first we find the actual value of E eigenvalue through root_scalar and
+later we solve the equations for that proper energy with solve_ivp.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from scipy.optimize import root_scalar
+import potential as pot
+import schro_equation as eq
+import get_values as get
+import find_energy as find
+
+# Find the value of E
+E_value = root_scalar(find.error_E, method='secant', x0=get.energy_guess())
+E_binding = E_value.root
+print(E_binding)
+
+# Get the eigenfunction with the proper E value
+solution_final = solve_ivp(lambda r, y: eq.radial_equation(r, y, E_binding),
+                           get.range_of_radius(), get.initial_conditions(),
+                           method = 'RK45', max_step = 0.01)
+
+# To visualize u(r) vs. r and V(r)
+r_values = solution_final.t
+u_values = solution_final.y[0]
+V_values = [pot.V_c_squarewell(r) for r in r_values]
+
+# Graph of function u(r), V(r) and E
+plt.figure()
+plt.plot(r_values, 20 * u_values, label='u(r)')
+plt.plot(r_values, V_values, label='Potential V(r) (MeV)')
+plt.axhline(y=E_binding, color='r', linestyle='--', label='Binding energy E (MeV)')
+plt.xlabel('r (fm)')
+plt.ylabel('u(r)')
+plt.title('Eigenfunction u(r) vs. ditance r')
+plt.legend()
+plt.grid(True)
+plt.show()
