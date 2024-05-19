f3ccfe7924871aa531a64529bd2f9339a5fe4004
hannasg22 <hannastammer22@gmail.com>
2024-05-19 11:47:13 +0200

    Improved the format for the tests and their documentation.

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-"""This module will contain all the tests to check that the code works.
+"""This file contains all the tests to check that the code works as it should.
 
 We will check each function, see that the given result is of the type we are
 expecting and that the functions are actually doing what they are designed for.
@@ -16,34 +16,34 @@ import find_energy as find
 # TESTS FOR get_values.py
 class TestValues(unittest.TestCase):
     """In each case we will make sure that the returned object is of the type
-    we expect it. These will be the expected outputs' forms in order to use
-    them in the rest of the code.
+    we expect. These will be the expected outputs' forms in order to use them
+    in the rest of the files.
     """
 
     def test_central_V(self):
         result = get.central_V()
         # Tests if the function returns a list
         self.assertIsInstance(result, list)
-        # Tests if the dimension of the output is the expected one
+        # Tests if the dimension of the output is the expected one, 2
         self.assertEqual(len(result), 2)
     
     def test_initial_conditions(self):
         result = get.initial_conditions()
         # Tests if the function returns a list
         self.assertIsInstance(result, list)
-        # Tests if the dimension of the output is the expected one
+        # Tests if the dimension of the output is the expected one, 2
         self.assertEqual(len(result), 2)
 
     def test_boundary_condition(self):
         result = get.boundary_condition()
-        # Tests if the function returns a tuple
+        # Tests if the function returns a float
         self.assertIsInstance(result, float)
         
     def test_range_of_radius(self):
         result = get.range_of_radius()
         # Tests if the function returns a list
         self.assertIsInstance(result, list)
-        # Tests if the dimension of the output is the expected one
+        # Tests if the dimension of the output is the expected one, 2
         self.assertEqual(len(result), 2)
         
     def test_energy_guess(self):
@@ -54,20 +54,23 @@ class TestValues(unittest.TestCase):
 # TESTS FOR potential.py
 class TestCentralPotential(unittest.TestCase):
     """We extract the depth and range of the potential from the real data and
-    compare the results we expect with the ones the function V_c_squarewell
-    returns.
+    compare the results we expect with the ones the function V_c_squarewell is
+    returning.
     """
+    
     @given(r_values=st.data())
     @settings(max_examples=20)
     def test_V_c_squarewell(self, r_values):
-        # Get actual values for potential depth and range from data file
+        # Get real values for the potential depth and range from the data file
         V0_c, r0_c = get.central_V()      
-        # Take different probe values of r
+        # Make different probe values of r
         r = r_values.draw(st.floats(min_value=0.0, max_value=10.0))
+        # Define the value we expect the function to return
         if r <= r0_c:
             V_expected = -V0_c
         else:
             V_expected = 0.0
+        # Define the value the function is returning
         result = pot.V_c_squarewell(r)
         # Tests if the function returns the expected V value
         error_msg = f"""Error when calculating the potential value for r = {r},
@@ -76,9 +79,10 @@ class TestCentralPotential(unittest.TestCase):
 
 # TEST FOR schro_equation.py
 class TestSchroedingerEquation(unittest.TestCase):
-    """We just test if the function returns a list and if the length is the
-    one we can expect.
+    """We just test if the function returns a list of floats and if the length
+    is the one we expect.
     """
+    
     @given(st.floats(min_value=0.1, max_value=10.0), # r
            st.lists(st.floats(min_value=-1.0, max_value=1.0),
                     min_size=2, max_size=2), # y
@@ -87,12 +91,11 @@ class TestSchroedingerEquation(unittest.TestCase):
     @settings(max_examples=5)
     def test_radial_equation(self, r, y, E):
         result = eq.radial_equation(r, y, E)
-        
         # Tests if the function returns a list
         self.assertIsInstance(result, list)
-        # Tests if the size of the function is the one expected
+        # Tests if the dimension of the output is the expected one, 2
         self.assertEqual(len(result), 2)
-        # Tests that the type of object returned is the one expected
+        # Tests that the type of objects returned is the expected one
         self.assertIsInstance(result[0], (float, np.float64))
         self.assertIsInstance(result[1], (float, np.float64))
 
@@ -100,12 +103,13 @@ class TestSchroedingerEquation(unittest.TestCase):
 class TestErrorE(unittest.TestCase):
     """We test that for the actual value of the eigenvalue we get a small
     error. This means that the function will find the root near that result.
-    BEWARE: the rest of the conditions must be also appropriate.
+    BEWARE: the rest of the conditions must also be appropriate.
     """
+    
     def test_error_E(self):
-        # Define the actual value of the binding energy for deuteron
+        # Define the real value of the eigenvalue of deuteron
         E_real = -2.25 # MeV
-        # Error value with real binding energy
+        # Error value with real E value
         error_min = abs(find.error_E(E_real))
         # Tolerance we accept for the error
         eps = 0.1
