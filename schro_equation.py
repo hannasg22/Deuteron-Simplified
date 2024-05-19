bfb57e5614527968bde7486d917388681bd4ca21
hannasg22 <hannastammer22@gmail.com>
2024-05-19 11:53:35 +0200

    Improved the documentation and checked that everything is correct.

--- schro_equation.py
+++ schro_equation.py
@@ -1,12 +1,12 @@
 """This module contains the simplified deuteron equation we want to solve to
-get the eigenvalue (only bound state of the system) and eigenfunction.
+get the eigenvalue (only bound state of the system) and the eigenfunction.
 
 Important information about units of measurements:
     Units used:
         - Distances: fm
         - Mass: MeV / c**2
         - Energy: MeV
-    We will define the reduced Planck constant with the next dimensions:
+    We defined the reduced Planck constant with the next dimensions:
         h_bar * c = 197.327 MeV * fm
 """
 
@@ -17,21 +17,23 @@ import potential as pot
 
 def radial_equation(r, y, E):
     """This function defines from our 2nd order differential equation a system
-    of 2 1st order ones, generated to solve the problem.
+    of 2 1st order ODEs, generated to later solve the problem with SciPy.
 
-    We have the eigenfunction 'us' representing the l=0 s-wave.
-
-    We define:
-        dus: 1st derivative of the eigenfunction us will be named as vs
-        dvs: 2nd derivative of eigenfunction us (1st derivative of vs)
+    We have the eigenfunction 'us' representing the l=0 s-wave. From there, we
+    define:
+        dus: 1st derivative of the eigenfunction us, which will be named as vs
+        dvs: 2nd derivative of the eigenfunction us (1st derivative of vs)
     Inputs:
         r: distance between both nucleons
         E: energy value
-        y: vector with the variables [us, vs]
+        y: vector which will contain the variables [us, vs]
+    Output:
+        [dus, dvs]: the system of 1st order ODEs which will be solved with
+                    SciPy
     """
 
     us, vs = y
-    # Definition of parameters
+    # Definition of needed parameters
     h_bar = 197.327 # Reduced Planck constant
     M = 938.918 # 2 times the reduced mass of deuteron
 
