a2bc10323781ca38921e8f328cfb4fec63c45b70
hannasg22 <hannastammer22@gmail.com>
2024-05-19 12:23:36 +0200

    Improved documentation and format.

--- find_energy.py
+++ find_energy.py
@@ -1,11 +1,11 @@
-"""Here we define the root finding process.
+"""Here we define the root finding process to achieve the proper eigenvalue.
 
-We take the function from 'schro_equation.py', which has the system we need to
-solve. We then use an integration process that gives the function lg the system
-of equations with the given parameters.
+We take the function 'radial_equation' from schro_equation.py, which contains
+the system we want to solve. We then solve it inside the 'error_E' function
+through an integration method with the inserted energy value.
 
-So, to follow this process we will use the functions from SciPy:
-    - solve_ivp --> to solve the equation with the introcued energy value
+So, to carry out the integration we make use of the next function from SciPy:
+    - integrate.solve_ivp --> solves the system with the introduced E value
 """
 
 import numpy as np
@@ -16,7 +16,7 @@ import potential as pot
 import schro_equation as eq
 import get_values as get
 
-# DIFFERENCE BETWEEN EIGENFUNCTIONS WITH E_GUESS AND OUR BOUNDARY CONDITION
+# DIFFERENCE BETWEEN EIGENFUNCTION WITH E_GUESS AND OUR BOUNDARY CONDITION
 def error_E(E_guess):
     """This function analyses if the result obtained with E_guess matches the
     desired boundary condition.
@@ -25,23 +25,21 @@ def error_E(E_guess):
     result in the ending point with the desired boundary condition us_fin.
 
     Inputs:
-        E: value of energy for which we solve the equations
-
+        E_guess: value of energy for which we solve the equations
     Output:
         error: difference between the final point value with E_guess and the
                actual value we want to achieve
     """
-
-    # Get the conditions to solve the problem
+    # Get the conditions from the data file
     ini_cond = get.initial_conditions()
     us_fin = get.boundary_condition()
     r_range = get.range_of_radius()
 
-    # Solve for E_guess
+    # Solve system for E_guess
     solution_guess = solve_ivp(lambda r, y: eq.radial_equation(r, y, E_guess),
                                r_range, ini_cond, method='RK45', max_step=0.01)
-
-    # Difference between solution with E_guess and the actual value we want
+    
+    # Difference between solution with E_guess and the boundary condition
     error = solution_guess.y[0][-1] - us_fin
 
     return error
