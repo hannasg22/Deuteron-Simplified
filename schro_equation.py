9cf99b38e2f05a41d18182b994d2d5a6bbf7daa2
hannasg22 <hannastammer22@gmail.com>
2024-05-20 09:51:55 +0200

    Tried to follow the PEP 8 format as much as possible.

--- schro_equation.py
+++ schro_equation.py
@@ -1,5 +1,5 @@
-"""This module contains the simplified deuteron equation we want to solve to
-get the eigenvalue (only bound state of the system) and the eigenfunction.
+"""This file contains the simplified deuteron equation we must solve to
+get the eigenvalue and the eigenfunction.
 
 Important information about units of measurements:
     Units used:
@@ -13,29 +13,32 @@ Important information about units of measurements:
 import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.optimize import root_scalar
+
 import potential as pot
 
-def radial_equation(r, y, E):
-    """This function defines from our 2nd order differential equation a system
-    of 2 1st order ODEs, generated to later solve the problem with SciPy.
 
-    We have the eigenfunction 'us' representing the l=0 s-wave. From there, we
-    define:
-        dus: 1st derivative of the eigenfunction us, which will be named as vs
-        dvs: 2nd derivative of the eigenfunction us (1st derivative of vs)
+def radial_equation(r, y, E):
+    """This function defines from our 2nd order differential equation a
+    system of 2 1st order ODEs, created to later solve the problem with
+    SciPy.
+
+    We have the eigenfunction 'us' representing the l=0 s-wave. From
+    there, we define:
+        dus: 1st derivative of us, which will be named as vs
+        dvs: 2nd derivative of us (1st derivative of vs)
     Inputs:
         r: distance between both nucleons
         E: energy value
         y: vector which will contain the variables [us, vs]
     Output:
-        [dus, dvs]: the system of 1st order ODEs which will be solved with
-                    SciPy
+        [dus, dvs]: the system of 1st order ODEs which will be solved
+                    with SciPy
     """
 
     us, vs = y
     # Definition of needed parameters
     h_bar = 197.327 # Reduced Planck constant
-    M = 938.918 # 2 times the reduced mass of deuteron
+    M = 938.918 # Twice the reduced mass of deuteron
 
     # System of equations we want to solve
     dus = vs
